0f9cc5f908501344e6d4093b9a22a28b9c5925a8
blessingmwalein <blessingmwalein@outlook.com>
2023-04-27 11:25:52 +0200

    add almost all features

--- app.py
+++ app.py
@@ -24,6 +24,42 @@ def register():
     return render_template('insert_card_fingerprint.html')
 
 
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users')
+
+    users = cursor.fetchall()
+    mysql.connection.commit()
+    if request.method == 'POST':
+        pin = request.form['pin']
+        # get accountnnumber from query string
+        accountnumber = request.form['account_number']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+        id_number = request.form['id_number']
+
+        print(accountnumber)
+        print(pin)
+        print(first_name)
+        print(last_name)
+        print(dob)
+        print(id_number)
+        
+
+        
+
+        newCursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        newCursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s,% s, % s, % s, % s)',
+                       (first_name, last_name, dob, id_number, pin, "0", accountnumber,))
+        mysql.connection.commit()
+
+        return render_template('users.html', users=users, msg="User Added")
+
+    return render_template('users.html', users=users, msg="")
+
+
 @app.route('/authenticate', methods=['GET', 'POST'])
 def authenticate():
     msg = ''
@@ -43,7 +79,9 @@ def authenticate():
             session['id'] = account['id']
             session['acount_number'] = account['account_number']
             msg = 'Logged in successfully !'
-            return render_template('account_detail.html', msg=msg)
+
+            account = getAcccount()
+            return render_template('account_detail.html', msg=msg, account=account)
         else:
             # return redirect with query string
             return redirect(
@@ -54,38 +92,60 @@ def authenticate():
 
 @app.route('/account', methods=['GET', 'POST'])
 def account():
-    account_number = session.get('acount_number')
-
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM users WHERE account_number = % s', (account_number,))
-    account = cursor.fetchone()
+    account = getAcccount()
 
     return render_template('account_detail.html', account=account)
 
 
 @app.route('/deposit', methods=['GET', 'POST'])
 def deposit():
-    return render_template('deposit.html')
+    account = getAcccount()
+    return render_template('deposit.html', account=account)
 
 
 @app.route('/deposit-cash', methods=['GET', 'POST'])
 def deposit_cash():
-    return render_template('deposit_cash.html')
+    account = getAcccount()
+    if request.method == 'POST' and 'amountToDeposit' in request.form:
+        accountnumber = request.form['amountToDeposit']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE users SET balance=balance + %s WHERE id=%s",
+                       (accountnumber, session.get('id'),))
+        mysql.connection.commit()
+        return render_template('deposit_cash.html', account=account, msg='Deposit successfully !')
+
+    return render_template('deposit_cash.html', account=account, msg='')
 
 
 @app.route('/deposit-check', methods=['GET', 'POST'])
 def deposit_check():
-    return render_template('deposit_check.html')
+    account = getAcccount()
+
+    return render_template('deposit_check.html', account=account)
 
 
 @app.route('/withdraw', methods=['GET', 'POST'])
 def withdraw():
-    return render_template('withdraw.html')
+    account = getAcccount()
+    if request.method == 'POST' and 'userPinInput' in request.form:
+        # convert string to float
+
+        userPinInput = float(request.form['userPinInput'])
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("UPDATE users SET balance=balance - %s WHERE id=%s",
+                       (userPinInput, session.get('id'),))
+        mysql.connection.commit()
+        return render_template('withdraw.html', account=account, msg='Withdraw successfully !')
+
+    return render_template('withdraw.html', account=account, msg="")
 
 
 @app.route('/transfer', methods=['GET', 'POST'])
 def transfer():
-    return render_template('transfer_money.html')
+    account = getAcccount()
+    return render_template('transfer_money.html', account=account)
 
 
 @app.route('/logout')
@@ -94,6 +154,14 @@ def logout():
     session.pop('id', None)
     session.pop('acount_number', None)
     return redirect(url_for('authenticate'))
+
+
+def getAcccount():
+    account_number = session.get('acount_number')
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users WHERE account_number = % s', (account_number,))
+    return cursor.fetchone()
 
 
 if __name__ == '__main__':
